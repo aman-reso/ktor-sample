e46e9782af96904c0119390b667298a940205eb1
Aman Kumar <amankumar@Amans-MacBook-Air.local>
2022-07-21 00:32:54 +0530

    okk

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: build/libs/ktor-sample-0.0.1-all.jar
--- build.gradle.kts
+++ build.gradle.kts
@@ -36,3 +36,6 @@ dependencies {
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
+tasks {
+    create("stage").dependsOn("installDist")
+}
